b2d2809db3e8d059099473f9e491896423d80221
Josh Talley <jtdev84@gmail.com>
2022-04-11 16:36:44 +0100

    Add CSS Grid layout for buttons

--- calc.js
+++ calc.js
@@ -13,3 +13,16 @@ const divide = function(num1, num2) {
 const multiply = function(num1, num2) {
     return num1 * num2
 };
+
+// const operate = function(operater, num1, num2) {
+//         if (operater === plus) {
+//             add();
+//         } else if (operater === minus) {
+//             subtract();
+//         } else if (operater === divide) {
+//             divide();
+//         } else if (operater === times) {
+//             multiply();
+//         };
+//     };
+// };
