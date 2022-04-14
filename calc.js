f60beec463a390405bf2aeb95a0de85e5cdbb2cd
Josh Talley <jtdev84@gmail.com>
2022-04-14 17:08:55 +0100

    Add button and output border in CSS, add calculator class and functions in JS

--- calc.js
+++ calc.js
@@ -1,74 +1,140 @@
-const b = document.querySelectorAll('button');
-const numbers = document.querySelectorAll('[data-number]');
-const operator = document.querySelectorAll('[data-operator]');
-const allClear = document.querySelector('[data-clear]');
-const decimal = document.querySelectorAll('[data-decimal]');
+// Class to store the operands and calculator functions
+class Calculator {
+    constructor(previousOperandText, currentOperandText) {
+        this.previousOperandText = previousOperandText
+        this.currentOperandText = currentOperandText
+        this.clear()
+    }
 
-// Event listeners to get value of button clicked
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
 
-const numberClicked = numbers.forEach(numbers => {
-    numbers.addEventListener('click', numberClick);
-    function numberClick(){
-        let numVal = this.textContent;
-        const numClickInt = parseInt(numVal);
-        console.log(numClickInt);
-        const display = document.querySelector('.display');
-        const h1 = document.createElement('h1');
-        h1.textContent = numClickInt
-        display.append(h1);
+    // Delete last number:
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
 
+    // Number that user clicks is appended to the display; this needs to take in the specific number:
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    // This happens when the user clicks on an operator. It needs to take in the specific operation:
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    // This take the values in the calculator and computes what number needs to be displayed:
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    // Helper function to set decimals in the correct places
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    // This updates the values displayed on the calculator:
+    updateDisplay() {
+        this.currentOperandText.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandText.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandText.innerText = ''
+        };
     };
-});
-
-operator.forEach(operator => {
-    operator.addEventListener('click', operatorClick);
-    function operatorClick(){
-        let opVal = this.textContent;
-        console.log(opVal);
-    };
-});
-
-decimal.forEach(decimal => {
-    decimal.addEventListener('click', decimalClick);
-    function decimalClick(){
-        let decClick = this.textContent;
-        console.log(decClick);
-    };
-});
-
-
-// allClear.forEach(allClear =>) {
-//     allClear.addEventListener('click', clearClick);
-//     function clearClick(){
-//         let clearAll
-//     }
-// }
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-const subtract = function(num1, num2) {
-	return num1 - num2
 };
 
-const divide = function(num1, num2) {
-	return num1 / num2
-};
+// Variables storing each of the buttons
 
-const multiply = function(num1, num2) {
-    return num1 * num2
-};
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const allClearButton = document.querySelector('[data-clear]');
+const equalsButton = document.querySelector('[data-equals');
+const delButton = document.querySelector('[data-delete');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
 
-// const operate = function(operater, num1, num2) {
-//         if (operater === plus) {
-//             add();
-//         } else if (operater === minus) {
-//             subtract();
-//         } else if (operater === divide) {
-//             divide();
-//         } else if (operater === times) {
-//             multiply();
-//         };
-//     };
-// };
+// Variable storing the calculator class
+
+const calculator = new Calculator(previousOperandText, currentOperandText)
+
+// Event listeners to register button clicks and update the display
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+delButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
