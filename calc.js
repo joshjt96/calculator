4f344ef2bb6aa61453c831b30a849806b15688b6
Josh Talley <jtdev84@gmail.com>
2022-04-14 17:17:23 +0100

    Clean up code, ready to add keyboard event listeners

--- calc.js
+++ calc.js
@@ -1,38 +1,52 @@
+// Variables storing each of the buttons
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const allClearButton = document.querySelector('[data-clear]');
+const equalsButton = document.querySelector('[data-equals');
+const delButton = document.querySelector('[data-delete');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
+
+// Variable storing the calculator class
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
 // Class to store the operands and calculator functions
 class Calculator {
     constructor(previousOperandText, currentOperandText) {
         this.previousOperandText = previousOperandText
         this.currentOperandText = currentOperandText
         this.clear()
-    }
+    };
 
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
-    }
+    };
 
     // Delete last number:
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
+    };
 
     // Number that user clicks is appended to the display; this needs to take in the specific number:
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
+    };
 
     // This happens when the user clicks on an operator. It needs to take in the specific operation:
     chooseOperation(operation) {
         if (this.currentOperand === '') return
         if (this.previousOperand !== '') {
             this.compute()
-        }
+        };
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
-    }
+    };
 
     // This take the values in the calculator and computes what number needs to be displayed:
     compute() {
@@ -55,11 +69,11 @@ class Calculator {
                 break
             default:
                 return
-        }
+        };
         this.currentOperand = computation
         this.operation = undefined
         this.previousOperand = ''
-    }
+    };
 
     // Helper function to set decimals in the correct places
     getDisplayNumber(number) {
@@ -72,69 +86,55 @@ class Calculator {
         } else {
             integerDisplay = integerDigits.toLocaleString('en', {
                 maximumFractionDigits: 0
-            })
-        }
+            });
+        };
         if (decimalDigits != null) {
             return `${integerDisplay}.${decimalDigits}`
         } else {
             return integerDisplay
-        }
-    }
+        };
+    };
 
     // This updates the values displayed on the calculator:
     updateDisplay() {
-        this.currentOperandText.innerText =
+        this.currentOperandText.textContent =
             this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
-            this.previousOperandText.innerText =
+            this.previousOperandText.textContent =
                 `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         } else {
-            this.previousOperandText.innerText = ''
+            this.previousOperandText.textContent = ''
         };
     };
 };
-
-// Variables storing each of the buttons
-
-const numberButtons = document.querySelectorAll('[data-number]');
-const operatorButtons = document.querySelectorAll('[data-operator]');
-const allClearButton = document.querySelector('[data-clear]');
-const equalsButton = document.querySelector('[data-equals');
-const delButton = document.querySelector('[data-delete');
-const previousOperandText = document.querySelector('[data-previous-operand]');
-const currentOperandText = document.querySelector('[data-current-operand]');
-
-// Variable storing the calculator class
-
-const calculator = new Calculator(previousOperandText, currentOperandText)
 
 // Event listeners to register button clicks and update the display
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText)
+        calculator.appendNumber(button.textContent)
         calculator.updateDisplay()
-    })
-})
+    });
+});
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText)
+        calculator.chooseOperation(button.textContent)
         calculator.updateDisplay()
-    })
-})
+    });
+});
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
-})
+});
 
 allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
-})
+});
 
 delButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
-})
+});
